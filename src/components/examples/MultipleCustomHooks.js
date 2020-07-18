19d1d43b60ceb42ceb1489aee66dd519b1a4d298
Erik Navarrete <leonavas1995@gmail.com>
2020-07-18 12:16:26 +0200

    component con varios customHooks

---
+++ src/components/examples/MultipleCustomHooks.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import '../02-useEffect/effects.css'
+import { useFetch } from '../../hooks/useFetch'
+import { useCounter } from '../../hooks/useCounter'
+
+export const MultipleCustomHooks = () => {
+
+
+	const { state: counter, increment } = useCounter(1);
+
+	const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+	const { author, quote } = Boolean(data) && data[0];		// utilzar !!null : transforma null a boolean, hace lo mismo que Boolean(null)
+
+	return (
+		<div>
+			<h1>Breakind Bad Quotes</h1>
+			{
+				loading
+					?
+					<div className="alert alert-info text-center">
+						Loading...
+					</div>
+					:
+					<>
+						<blockquote className="blockquote text-right">
+							<p className="mb-0">{quote}</p>
+							<footer className="blockquote-footer">{author}</footer>
+						</blockquote>
+						<button className="btn btn-outline-primary"
+							onClick={() => increment()}>
+							Next quote!
+						</button>
+					</>
+			}
+
+		</div>
+	)
+}
